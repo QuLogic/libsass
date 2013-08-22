0ffedeb0cfffcd7d16b6b7bc950adc41b5203fc7
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2013-08-22 03:32:20 -0400

    Enable coverage reports on Travis.
    I only bothered enabling this on the newer automake builds. Coverage
    testing requires g++ and the build with newer automake only uses it.
    Plus, coverage testing isn't going to change between automake versions
    anyway.

--- travis.sh
+++ travis.sh
@@ -6,16 +6,22 @@ if [ "x$AUTOTOOLS" == "xnewer" ]; then
 	sudo add-apt-repository -y ppa:rbose-debianizer/automake &> /dev/null
 	sudo apt-get -qq update
 	sudo apt-get -qq install automake
+	sudo pip install cpp-coveralls --use-mirrors
 	AUTOTOOLS=yes
+	COVERAGE=--enable-coverage
 fi
 
 if [ "x$AUTOTOOLS" == "xyes" ]; then
 	autoreconf
-	./configure --enable-tests \
+	./configure --enable-tests $COVERAGE \
 	            --with-sassc-dir=$SASS_SASSC_PATH \
 	            --with-sass-spec-dir=$SASS_SPEC_PATH
 	make
 fi
 
 make LOG_FLAGS=--skip VERBOSE=1 test
+
+if [ -n "$COVERAGE" ]; then
+	coveralls --exclude lib --exclude tests
+fi
 
