d0aa5b844c56a846b7cee301bcaa291d330431da
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2013-08-22 05:48:01 -0400

    Fold some messages on Travis.
    The coveralls output is really long, so it's nice to hide it. This may
    not be documented, but it works. :wink:

--- travis.sh
+++ travis.sh
@@ -3,19 +3,25 @@
 set -e
 
 if [ "x$AUTOTOOLS" == "xnewer" ]; then
-	sudo add-apt-repository -y ppa:rbose-debianizer/automake &> /dev/null
-	sudo apt-get -qq update
-	sudo apt-get -qq install automake
+	echo -en 'travis_fold:start:update_automake\r'
+	sudo add-apt-repository -y ppa:rbose-debianizer/automake
+	sudo apt-get update
+	sudo apt-get install automake
+	echo -en 'travis_fold:end:update_automake\r'
+	echo -en 'travis_fold:start:install_coveralls\r'
 	sudo pip install cpp-coveralls --use-mirrors
+	echo -en 'travis_fold:end:install_coveralls\r'
 	AUTOTOOLS=yes
 	COVERAGE=--enable-coverage
 fi
 
 if [ "x$AUTOTOOLS" == "xyes" ]; then
+	echo -en 'travis_fold:start:configure\r'
 	autoreconf
 	./configure --enable-tests $COVERAGE \
 	            --with-sassc-dir=$SASS_SASSC_PATH \
 	            --with-sass-spec-dir=$SASS_SPEC_PATH
+	echo -en 'travis_fold:end:configure\r'
 	make
 else
 	make ${SASS_SASSC_PATH}/bin/sassc
@@ -27,7 +33,9 @@ make LOG_FLAGS=--skip VERBOSE=1 AM_COLOR_TESTS=always test
 status=$?
 
 if [ -n "$COVERAGE" ]; then
+	echo -en 'travis_fold:start:coveralls\r'
 	coveralls --exclude .libs --exclude $SASS_SPEC_PATH
+	echo -en 'travis_fold:end:coveralls\r'
 fi
 
 exit $status
